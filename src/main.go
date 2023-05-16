f37ab50fec033201054578a9e8f87f630912df83
K_hitoshi <kubotah1115@gmail.com>
2023-05-16 09:28:15 +0900

    UDP受信処理追加

---
+++ src/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	// サーバーのIPアドレス
+	SERVER_IP = "localhost"
+	// サーバーのポート番号
+	SERVER_PORT = 1234
+	// バッファサイズ
+	BUFFER_SIZE = 1024
+)
+
+func main() {
+	// サーバーアドレスの生成
+	/*
+		serverAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", SERVER_PORT))
+		if err != nil {
+			fmt.Println("サーバーアドレスの生成に失敗しました:", err)
+			os.Exit(1)
+		}*/
+
+	serverAddr := &net.UDPAddr{
+		IP:   net.ParseIP(SERVER_IP),
+		Port: SERVER_PORT,
+	}
+
+	// ソケットの生成
+	conn, err := net.ListenUDP("udp", serverAddr)
+	if err != nil {
+		fmt.Println("ソケットの生成に失敗しました:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("サーバーが起動しました。")
+
+	// 受信ループ
+	for {
+		buffer := make([]byte, BUFFER_SIZE)
+		_, clientAddr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("データの受信中にエラーが発生しました:", err)
+			continue
+		}
+
+		message := string(buffer)
+		fmt.Printf("クライアントからのメッセージ [%s]: %s\n", clientAddr.String(), message)
+
+		// "exit"が受信されたら終了
+		if message == "exit" {
+			break
+		}
+	}
+
+	fmt.Println("サーバーが終了しました。")
+}
