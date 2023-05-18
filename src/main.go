34966cfd01c8e4fc5400302bdaa6d20a5dd43011
K_hitoshi <kubotah1115@gmail.com>
2023-05-19 00:56:40 +0900

    gorutineできるように切り分け

--- src/main.go
+++ src/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
 const (
@@ -17,20 +16,26 @@ const (
 	BUFFER_SIZE = 1024
 )
 
-type Data struct {
+// UDP通信で受け取るデータ
+type recvData struct {
 	Name    string `json:"name"`
 	Message string `json:"message"`
 }
 
-func main() {
-	// サーバーアドレスの生成
-	/*
-		serverAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", SERVER_PORT))
-		if err != nil {
-			fmt.Println("サーバーアドレスの生成に失敗しました:", err)
-			os.Exit(1)
-		}*/
+// ユーザーのパラメータ
+type UserParam struct {
+	//位置データ
+	//向いている方向
+	Message string
+}
 
+// プレイヤーと通信処理する構造体
+type PlayerConnection struct {
+	conn *net.UDPConn
+}
+
+// PlayerConnection コンストラクタ
+func NewPlayerConnection() (*PlayerConnection, error) {
 	//サーバーアドレスの生成
 	serverAddr := &net.UDPAddr{
 		IP:   net.ParseIP(SERVER_IP),
@@ -40,44 +45,75 @@ func main() {
 	// ソケットの生成
 	conn, err := net.ListenUDP("udp", serverAddr)
 	if err != nil {
-		fmt.Println("ソケットの生成に失敗しました:", err)
-		os.Exit(1)
+		//fmt.Println("ソケットの生成に失敗しました:", err)
+		//log.Fatal("ソケットの生成に失敗しました:", err)
+		return nil, err
 	}
-	defer conn.Close()
 
+	//生成したソケットを含めて返す
+	return &PlayerConnection{conn: conn}, err
+}
+
+// PlayerConnection 更新処理
+func (pc *PlayerConnection) UpdatePlayerConnection() {
 	fmt.Println("サーバーが起動しました。")
 
+	//ユーザーのパラメーター
+	userParam := map[string][]UserParam{}
 	for {
 		//受信
 		buffer := make([]byte, BUFFER_SIZE)
-		_, clientAddr, err := conn.ReadFromUDP(buffer)
+		recvbyte, clientAddr, err := pc.conn.ReadFromUDP(buffer)
 		if err != nil {
-			fmt.Println("データの受信中にエラーが発生しました:", err)
+			log.Println("データの受信中にエラーが発生しました: ", err)
 			continue
 		}
 
-		//message := string(buffer)
-		//fmt.Printf("クライアントからのメッセージ [%s]: %s\n", clientAddr.String(), message)
-
-		//データが小さかったり大きかったりするとデータの最後にNULLなどが入りエラーが発生するため
-		//jsonデータをトリミングする
-		trimmedBuffer := bytes.Trim(buffer, "\x00")
-		var d Data
-		err = json.Unmarshal(trimmedBuffer, &d)
+		//受信したデータを構造体形式に変換する
+		var recvdata recvData
+		err = json.Unmarshal(buffer[:recvbyte], &recvdata)
 		if err != nil {
-			fmt.Println(err)
-			return
+			log.Println("jsonデータの変換に失敗: ", err)
+			continue
 		}
+
+		//受信したデータを登録
+		userParam[recvdata.Name] = append(userParam[recvdata.Name], UserParam{Message: recvdata.Message})
+
 		//output
-		fmt.Println("Name:", d.Name)
-		fmt.Println("Message:", d.Message)
+		fmt.Println(userParam)
+
+		//TODO:デバック時のみ有効にする
+		//終了宣言のチェック
+		if recvdata.Message == "exit" {
+			break
+		}
 
 		//送信
-		_, err = conn.WriteToUDP(buffer, clientAddr)
+		_, err = pc.conn.WriteToUDP(buffer, clientAddr)
 		if err != nil {
-			fmt.Println("read error: ", err)
+			log.Println("read error: ", err)
+			continue
 		}
 	}
+}
 
+func main() {
+	//プレイヤーとの通信する構造体生成
+	pc, err := NewPlayerConnection()
+	if err != nil {
+		log.Fatal("PlayerConnectionの生成に失敗: ", err)
+	}
+
+	c := make(chan bool, 1)
+	//更新処理
+	go func() {
+		pc.UpdatePlayerConnection()
+		c <- true
+	}()
+	//待機
+	<-c
+	pc.conn.Close()
+	//defer conn.Close()
 	fmt.Println("サーバーが終了しました。")
 }
