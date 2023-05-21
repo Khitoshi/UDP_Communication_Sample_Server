4187707ef829171c061031a0669523b2b6c582a7
K_hitoshi <kubotah1115@gmail.com>
2023-05-22 02:58:02 +0900

    ルーム用gorutineを作成するための前処理(環境構築&protoのバージョンの確認)完了

--- src/main.go
+++ src/main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 
-	services "UDPserver/src/protocode"
+	services "github.com/Khitoshi/UDP_Communication_Sample_Server/src/pb"
 
 	"google.golang.org/grpc"
 )
 
 type myServer struct {
-	services.UnimplementedGreetingServiceServer
+	services.UnimplementedLoginServiceServer
+}
+
+// 自作サービス構造体のコンストラクタを定義
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+// Unary RPCがレスポンスを返すところ
+// ログイン処理を実行
+// TODO:インターセプター処理の追加
+func (m *myServer) Login(ctx context.Context, req *services.LoginRequest) (*services.LoginResponse, error) {
+	/*
+		//ctxからメタデータを取得
+		if md, ok := metadata.FromIncomingContext(ctx); ok {
+			log.Println(md)
+		}
+
+		//ヘッダーを作成
+		headerMD := metadata.New(map[string]string{"type": "unary", "from": "server", "in": "header"})
+		if err := grpc.SetHeader(ctx, headerMD); err != nil {
+			return nil, err
+		}
+
+		//トレイラーを作成
+		trailerMD := metadata.New(map[string]string{"type": "unary", "from": "server", "in": "trailer"})
+		if err := grpc.SetTrailer(ctx, trailerMD); err != nil {
+			return nil, err
+		}
+	*/
+	// HelloResponse型を1つreturnする
+	// (Unaryなので、レスポンスを一つ返せば終わり)
+	return &services.LoginResponse{
+		Id:   fmt.Sprintf("0: %s", req.GetName()),
+		Name: req.GetName(),
+	}, nil
 }
 
 func main() {
@@ -43,7 +79,7 @@ func main() {
 	s := grpc.NewServer()
 
 	//gRPCサーバーにGreetingServiceを登録
-	hellopb.RegisterGreetingServiceServer(s)
+	services.RegisterLoginServiceServer(s, NewMyServer())
 
 	// 作成したgRPCサーバーを、8080番ポートで稼働させる
 	go func() {
