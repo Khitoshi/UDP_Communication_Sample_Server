2a57db17bbecadcb0b9112d4d220c0b8ab78ba61
K_hitoshi <kubotah1115@gmail.com>
2023-05-22 01:12:06 +0900

    ファイル名変更

--- src/main.go
+++ src/main.go
@@ -3,20 +3,59 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	services "UDPserver/src/protocode"
+
+	"google.golang.org/grpc"
 )
+
+type myServer struct {
+	services.UnimplementedGreetingServiceServer
+}
 
 func main() {
 
-	server, err := NewServer()
+	/*
+		server, err := NewServer()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = server.Listen()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//server終了
+		fmt.Println("server終了")
+	*/
+
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	err = server.Listen()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// gRPCサーバーを作成
+	s := grpc.NewServer()
 
-	//server終了
-	fmt.Println("server終了")
+	//gRPCサーバーにGreetingServiceを登録
+	hellopb.RegisterGreetingServiceServer(s)
+
+	// 作成したgRPCサーバーを、8080番ポートで稼働させる
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	// 4.Ctrl+Cが入力されたらGraceful shutdownされるようにする
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+
 }
